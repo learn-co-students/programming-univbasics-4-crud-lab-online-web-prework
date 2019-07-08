07ef4208e5ae80f71df13614f760dce7b8b9ddcf
Calvin McDowell <calvinmac40@hotmail.com>
2019-07-08 16:01:55 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,28 +1,39 @@
 def create_an_empty_array
-  
+  []
 end
 
-def create_an_array
+def create_an_array["Big Bank", "Take", "Little", "Bank"]
   
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ["Take", "Little", "Bank"]
+  add_element_to_end_of_array << "Big Bank" 
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  [ "Take", "Little", "Bank"]
+  add_element_to_start_of_array.unshift("Big Bank")
+  p add_element_to_start_of_array
 end
 
 def remove_element_from_end_of_array(array)
+  ["Big Bank", "Take", "Little", "Bank"]
+  bank_array = remove_element_from_end_of_array.pop
   
+  p remove_element_from_end_of_array
+  p bank_array
 end
 
 def remove_element_from_start_of_array(array)
+  ["Big Bank", "Take", "Little", "Bank"]
+  big_bank = remove_element_from_start_of_array.shift
   
+  p remove_element_from_start_of_array
+  p big_bank
 end
 
-def retrieve_element_from_index(array, index_number)
+def retrieve_element_from_index = ["Big Bank", "Take", "Little", "Bank"]
   
 end
 
