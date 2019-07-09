6d968420b4c7f1199e3ca46b5612655950351716
Calvin McDowell <calvinmac40@hotmail.com>
2019-07-09 00:00:30 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,50 +1,42 @@
 def create_an_empty_array
   []
-end
+  end
 
-def create_an_array["Big Bank", "Take", "Little", "Bank"]
-  
-end
+def create_an_array
+  array = ["Big Bank", "Take", "Little", "Bank"]
+  end
 
 def add_element_to_end_of_array(array, element)
-  ["Take", "Little", "Bank"]
-  add_element_to_end_of_array << "Big Bank" 
+  array.push(element) 
 end
 
 def add_element_to_start_of_array(array, element)
-  [ "Take", "Little", "Bank"]
-  add_element_to_start_of_array.unshift("Big Bank")
-  p add_element_to_start_of_array
-end
+  array << "element" 
+end 
 
 def remove_element_from_end_of_array(array)
-  ["Big Bank", "Take", "Little", "Bank"]
-  bank_array = remove_element_from_end_of_array.pop
-  
-  p remove_element_from_end_of_array
-  p bank_array
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  ["Big Bank", "Take", "Little", "Bank"]
-  big_bank = remove_element_from_start_of_array.shift
-  
-  p remove_element_from_start_of_array
-  p big_bank
-end
+ array = remove_element_from_start_of_array.shift
+ end
 
-def retrieve_element_from_index = ["Big Bank", "Take", "Little", "Bank"]
-  
+def retrieve_element_from_index(array)
+  retrieve_element_from_index.index(array)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array.last
 end
 
 def update_element_from_index(array, index_number, element)
-
+  update_element_from_index[2] = "index number"
 end
+
+
+p update_element_from_index
