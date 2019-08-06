a54a793dc08fa5e17bc47d54e140c48be2f431c4
Hannah Harrington <hannahharrington@outlook.com>
2019-08-06 23:43:13 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,49 @@
 def create_an_empty_array
-  
+    []
 end
 
 def create_an_array
-  
+  Family = Array ["Kornell", "Mya", "Gma","Papa", "GiGi"]
+  puts Family
 end
 
 def add_element_to_end_of_array(array, element)
+  Family = Array ["Kornell", "Mya", "Gma","Papa", "GiGi"]
+  puts Family
   
 end
 
 def add_element_to_start_of_array(array, element)
-  
+   Family = Array ["Kornell", "Mya", "Gma","Papa", "GiGi"]
+  Family.unshift ("AO")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  Family = Array ["Kornell", "Mya", "Gma","Papa", "GiGi"]
+  pop(2)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  Family = Array ["Kornell", "Mya", "Gma","Papa", "GiGi"]
+  pop(0)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+Family = Array ["Kornell", "Mya", "Gma","Papa", "GiGi"]
+  puts Family [0]
 end
 
 def retrieve_first_element_from_array(array)
-  
+   Family = Array ["Kornell", "Mya", "Gma","Papa", "GiGi"]
+   print Family [1];
 end
 
 def retrieve_last_element_from_array(array)
-  
+   Family = Array ["Kornell", "Mya", "Gma","Papa", "GiGi"]
+   print Family [2];
 end
 
 def update_element_from_index(array, index_number, element)
-
+  Family = Array ["Kornell", "Mya", "Gma","Papa", "GiGi"]
+  puts Family [0] = "Cornell"
 end
