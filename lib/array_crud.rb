418e4395e7242df57c2f7b2ea28aaa4f85f29dfc
Lauren Dove <elledoveinc@gmail.com>
2019-08-10 18:00:04 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,58 @@
 def create_an_empty_array
+  []
   
 end
 
 def create_an_array
-  
+ my_pets = ["pepper" , "chloe" , "dashal" , "cupcake"]
+ 
 end
 
 def add_element_to_end_of_array(array, element)
+  my_pets = ["pepper" , "chloe" , "dashal" , "cupcake"]
   
+   my_pets =  << "piggy smalls"
 end
 
 def add_element_to_start_of_array(array, element)
+  my_pets = ["pepper" , "chloe" , "dashal" , "cupcake"]
   
+my_pets.unshift("goldie")  
 end
 
 def remove_element_from_end_of_array(array)
+  my_pets = ["pepper" , "chloe" , "dashal" , "cupcake"]
   
+  cupcake = my_pets.pop
 end
 
 def remove_element_from_start_of_array(array)
+  my_pets = ["pepper" , "chloe" , "dashal" , "cupcake"]
   
+  pepper = my_pets.shift
 end
 
 def retrieve_element_from_index(array, index_number)
+  my_pets = ["pepper" , "chloe" , "dashal" , "cupcake"]
   
+  my_pets[2]
 end
 
 def retrieve_first_element_from_array(array)
+  my_pets = ["pepper" , "chloe" , "dashal" , "cupcake"]
   
+  my_pets[0]
 end
 
 def retrieve_last_element_from_array(array)
+  my_pets = ["pepper" , "chloe" , "dashal" , "cupcake"]
   
+  my_pets[-1]
 end
 
 def update_element_from_index(array, index_number, element)
+  my_pets = ["pepper" , "chloe" , "dashal" , "cupcake"]
+  
+  my_pets[1] = "piggy smalls"
 
 end
