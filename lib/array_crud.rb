d94db8c1f3caaa149cd6a869373683c167d635e1
Richell Bonfield <richell.bonfield12@gmail.com>
2019-08-21 03:00:20 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,68 @@
 def create_an_empty_array
+  my_array = []
+end
+
+
+def create_an_array
+  my_array = ["Apple", "Banana", "Cherry", "Dates"]
   
 end
 
-def create_an_array
-  
-end
 
 def add_element_to_end_of_array(array, element)
   
+  my_array = ["Apple", "Banana", "Cherry", "Dates"]
+  my_array << element
+  my_array
+  
 end
+
 
 def add_element_to_start_of_array(array, element)
   
+  array.unshift(element)
+  
 end
+
 
 def remove_element_from_end_of_array(array)
   
+  array.pop
+  
 end
+
 
 def remove_element_from_start_of_array(array)
   
+  array.shift
+  
 end
+
 
 def retrieve_element_from_index(array, index_number)
   
-end
-
-def retrieve_first_element_from_array(array)
+  array[index_number]
   
 end
+
+
+def retrieve_first_element_from_array(array)
+ 
+  array[0]
+  
+end
+
+
 
 def retrieve_last_element_from_array(array)
   
+  array[-1]
+  
 end
 
+
 def update_element_from_index(array, index_number, element)
+  
+  array[index_number] = element
 
 end
