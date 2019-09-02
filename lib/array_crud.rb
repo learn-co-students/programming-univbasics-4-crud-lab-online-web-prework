849052387c2144b8b7c75dfc6827119fbd409b88
Jamal Turner <jlsmitty2273@yahoo.com>
2019-09-02 21:37:49 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,15 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+toppings = ["bacon", "cheese", "onions", "ham"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+array = ["wow", "I", "am", "really", "learning"]
+element = "arrays"
+array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
