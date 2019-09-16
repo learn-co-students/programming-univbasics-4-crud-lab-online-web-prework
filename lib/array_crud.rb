cc016fe37d67d36f7e2056380d84b4d77f165b2c
splendid-unit-0788 <jaredmadison@icloud.com>
2019-09-16 00:09:50 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,48 @@
 def create_an_empty_array
+  [ ]
   
 end
 
-def create_an_array
+def directions
+  directions = [north, south, east, west]
   
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  directions << (directions, "northwest")
 end
 
 def add_element_to_start_of_array(array, element)
+  directions.unshift(directions, "southwest")
   
 end
 
 def remove_element_from_end_of_array(array)
+  (directions.pop)
   
 end
 
 def remove_element_from_start_of_array(array)
-  
+  (directions.shift)
 end
 
 def retrieve_element_from_index(array, index_number)
+  (directions, [3])
   
 end
 
 def retrieve_first_element_from_array(array)
+  (directions)[0]
   
 end
 
 def retrieve_last_element_from_array(array)
+  (directions)[4]
   
 end
 
 def update_element_from_index(array, index_number, element)
+  (directions, ['3'], east)
+  
 
 end
