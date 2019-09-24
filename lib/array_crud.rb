54f6bb8e80de1b29d36c464c865964d408a68769
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-09-24 00:19:49 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,24 @@
 def create_an_empty_array
-  
+  []
 end
 
-def create_an_array
-  
+def create_an_array 
+  ["I", "Am", "Really", "Learning"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+create_an_array = ["I", "Am", "Really", "Learning"]
+create_an_array << "arrays!"
+create_an_array.unshift("wow")
+create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
