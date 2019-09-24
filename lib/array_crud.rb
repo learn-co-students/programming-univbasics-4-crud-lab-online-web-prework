4cd8eebd439fa9121a72a944a237b3cfec90b301
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-09-24 05:03:39 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,45 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  puppy = ["Pitbull", "Pug", "Boxer", "German Sheppard"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  result = [array]
+  result << element 
+  p result
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  result = [array]
+  result.unshift (element)
+  p result
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = array.pop
+  p array 
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = array.shift 
+  p array 
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0] 
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+retrieve_element_from_index(array) = [index_number, element]
 end
