5d800cc0299b6e42e4e0811693ff4f5015ee6aa3
Maika Kailani <queenmab051@gmail.com>
2019-10-23 00:23:08 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,56 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  planets = ["Manaan", "Kashyyk", "Coruscant", "Taris"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
-end
+  planets << "Korriban"
+  p planets
+end1
 
 def add_element_to_start_of_array(array, element)
-  
+  planets.unshift ("Nar Shadaa")
+  p planets
 end
 
 def remove_element_from_end_of_array(array)
-  
+  taris_planet = planets.pop
+  p planets
+  p taris_planet
 end
 
 def remove_element_from_start_of_array(array)
-  
+  manaan_planet = planets.shift
+  p planets
+  p manaan_planet
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  planets [0]
+  #=> "Manaan"
+  planets [1]
+  #=> "Kashyyk"
+  planets [2]
+  #=> "Coruscant"
+  planets [3]
+  #=> "Taris"
 end
 
 def retrieve_first_element_from_array(array)
-  
+  planets["Manaan"]
+  p planets[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  planets["Taris"]
+  p planets[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  planets[3] = "Balmorra"
+  => "Balmorra"
+  p planets
 end
