0e2ea356c33034af8389e23f8c2f3dba0ce0b4ae
qiong sun <joannesun44@gmail.com>
2019-10-25 21:05:53 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,26 +1,35 @@
-def create_an_empty_array
+def create_an_empty_array = []
   
 end
 
-def create_an_array
+def My_words ["good", "better", "best", "great"]
   
 end
 
-def add_element_to_end_of_array(array, element)
+def My_words.push("owesome")
   
 end
 
-def add_element_to_start_of_array(array, element)
+def My_words.unshift("owesome")
   
 end
 
-def remove_element_from_end_of_array(array)
+def My_words.pop = great_word
   
 end
 
-def remove_element_from_start_of_array(array)
+def My_words.shift = good
   
 end
+
+My_words [0]
+My_words [1]
+My_words [2]
+My_words [3]
+
+My_words [-1]
+
+My_words [3] = "bad"
 
 def retrieve_element_from_index(array, index_number)
   
