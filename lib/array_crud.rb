688ae106b6d5729333b381c79a9b917efb52a44b
Taishawn King <taishawnking1@yahoo.com>
2019-10-26 18:05:44 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,33 +1,39 @@
-def create_an_empty_array
-  
-end
+def 
+  create_an_empty_array []
+  p create_an_empty_array
+end 
 
 def create_an_array
-  
+  ["taishawn","yulemy","hot mess", "da butts"]
+  p create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array.push("chicken")
+  p add_element_to_end_of_array
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  add_element_to_start_of_array.unshift("mojo")
+  p add_element_to_start_of_array
 end
 
 def remove_element_from_end_of_array(array)
-  
+ remove_element_from_end_of_array= remove_element_from_end_of_array.pop
+ p remove_element_from_end_of_array
 end
 
 def remove_element_from_start_of_array(array)
-  
+  remove_element_from_start_of_array = remove_element_from_start_of_array.shift
+  p remove_element_from_start_of_array
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  retrieve_element_from_index[3]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  retrieve_first_element_from_array[0]
 end
 
 def retrieve_last_element_from_array(array)
