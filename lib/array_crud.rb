c525e0917552cb5c5caa858ac78a047bdbe90805
dwane richards <dwanerich@gmail.com>
2019-11-03 21:51:53 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,54 +1,51 @@
-def create_an_empty_array = []
+def create_an_empty_array
+  array = []
   
 end
 
-def create_an_array = ["dwane", "yessenia", "robbie", "celly", "max"]
+def create_an_array
+  array = ["dwane", "yessenia", "robbie", "celly"]
   
 end
 
 def add_element_to_end_of_array(array, element)
-    new_born = "baby"
-    create_an_array.push("baby")
+    
+    array.push('baby')
 end
 
 def add_element_to_start_of_array(array, element)
   
-    ma_ma = "Grandma"
-  create_an_array.unshift("Grandma")
+  array.unshift("Grandma")
   
 end
 
 def remove_element_from_end_of_array(array)
   
-  new_born = "baby"
-  create_an_array.pop
+  array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
   
-  top_ele = create_an_array[0]
-  create_an_array.shift
+  array.shift
   
 end
 
 def retrieve_element_from_index(array, index_number)
   
-  create_an_array[1]
+  array[1]
   
 end
 
 def retrieve_first_element_from_array(array)
-  create_an_array[0]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-    create_an_array[-1]
+    array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-  create_an_array
-  create_an_array[2] = "Middle Child Syndrome"
-  
-  p create_an_array
+  array[2] = "Middle Child Syndrome"
+  p array[2]  
 end
