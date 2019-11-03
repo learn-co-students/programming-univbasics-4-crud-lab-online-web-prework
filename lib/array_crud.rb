d32d0898bf7ab4153ff426b40b7f79e1edb7619a
dwane richards <dwanerich@gmail.com>
2019-11-03 22:02:03 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,12 +10,12 @@ end
 
 def add_element_to_end_of_array(array, element)
     
-    array.push('baby')
+    array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
   
-  array.unshift("Grandma")
+  array.unshift(element)
   
 end
 
@@ -32,9 +32,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
-  array[1]
-  
+    array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
@@ -47,5 +45,4 @@ end
 
 def update_element_from_index(array, index_number, element)
   array[2] = "totally"
-  p array[2]  
 end
