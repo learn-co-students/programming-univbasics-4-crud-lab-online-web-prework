3e76ea5064648cdeaf117f1004f290ff0968ab17
Emilee Sudweeks <emilee.sudweeks@wework.com>
2019-11-05 16:16:33 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,34 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  array = ["wow", "I", "am", "really"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["wow", "I", "am", "really"]
+  element = .push[dumb]
+end 
+
+def add_element_to_start_of_array
+  array = ["wow", "I", "am", "really"]
+  element.unshift = [dumb]
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  [Kaili, Ashley, Emilee, Natalie]
+  add_element_to_start_of_array.push (Laura)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  [Kaili, Ashley, Emilee, Natalie]
+  remove_element_to_start_of_array.shift (Laura)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  [Kaili, Ashley, Emilee, Natalie]
+  remove_element_to_start_of_array.shift (Laura)
 end
 
 def retrieve_element_from_index(array, index_number)
