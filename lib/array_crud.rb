2169c4e19c0ecd978ea1b8e5168b514b601c7757
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-03 22:28:34 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,48 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["north", "east", "south", "west"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  ending = array.pop
+  p ending
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  beginging = array.shift 
+  p beginging
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  p array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  p array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  p array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[4]="totally"
+  p array[4]
 end
