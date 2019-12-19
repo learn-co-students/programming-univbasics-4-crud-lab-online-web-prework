71f3d999b35052aacea278bb7d9afea87b5123dc
Agnes Park <agnespark824@gmail.com>
2019-12-19 02:09:58 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,39 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["a", "c", "c", "d"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(create_an_array, element)
+  create_an_array << element
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(create_an_array, element)
+  create_an_array.unshift("f")
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array(create_an_array)
+  create_an_array.pop
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array(create_an_array)
+  create_an_array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(create_an_array, 2)
+  create_an_array[2]
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array(create_an_array)
+  create_an_array[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array(create_an_array)
+  create_an_array[-1]
 end
 
-def update_element_from_index(array, index_number, element)
-
+def update_element_from_index(create_an_array, 0, "z")
+  create_an_array[0] = "z"
 end
